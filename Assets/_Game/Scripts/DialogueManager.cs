f396bfcb0dea6624a25bbeb2173bb3ae9b000602
Lmaclittle <ylmacias13@gmail.com>
2024-04-23 22:36:19 -0500

    Added Color Options
    Added the ability for people to choose colors for: Dialogue, NPC Name, and Dialogue Box. Also added a border. Need to add sound and reactionary text.

--- Assets/_Game/Scripts/DialogueManager.cs
+++ Assets/_Game/Scripts/DialogueManager.cs
@@ -6,24 +6,39 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    [Header("The basics")]
+    [Header("Text settings")]
     [Tooltip("Put the TMPro name of NPC in here for it to be displayed")]
     public TextMeshProUGUI _nameText;
+    [Tooltip("Use this to adjust text color and opacity of the text related to the NPC's name")]
+    [SerializeField] private Color _nameColor;
     [Tooltip("Put the TMPro dialogue of NPC in here for it to be displayed")]
     public TextMeshProUGUI _dialogueText;
+    [Tooltip("Use this to adjust text color and opacity of the text related to the NPC's dialogue")]
+    [SerializeField] private Color _dialogueTextColor;
 
     [Header("Dialogue box settings")]
+    [Tooltip("Place the dialogue box in here to access it. MUST BE A IMAGE.")]
+    public Image _dialogueBox;
+    [Tooltip("Use this to change the color and opacity of the dialogue box")]
+    [SerializeField] private Color _dialogueBoxColor;
     [Tooltip("Affects how fast text moves, make the number high to go slower, make it low to go faster.")]
     public float _characterDelay = 0.05f;
-
     [Tooltip("Animator used to make the dialogue box appear and disappear via a side swipe")]
     public Animator _animator;
+    
 
     public Queue<string> sentences;
 
     void Start()
     {
         sentences = new Queue<string>();
+    }
+
+    private void Update()
+    {
+        _nameText.color = _nameColor;
+        _dialogueText.color = _dialogueTextColor;
+        _dialogueBox.color = _dialogueBoxColor;
     }
 
     public void StartDialogue (Dialogue dialogue)
